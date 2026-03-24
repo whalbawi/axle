54270efcee3cb226ac510c771856e0a9f93ba812
Wael Halbawi <waelhalbawi@gmail.com>
2026-03-24 14:44:26 -0700

    build: Run CI jobs on Linux
    Supports Ubuntu and runs on 24.04 arm runners.
    
    Uncovered a read/write race against errno in `poller_linux.cpp`.

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -5,6 +5,14 @@ OS=$(uname)
 if [ $OS = "Darwin" ]; then
     echo "Running on macOS"
     . $SRC_DIR/setup-darwin.sh
+elif [ $OS = "Linux" ]; then
+    echo "Running on Linux"
+    if [ -f /etc/os-release ]; then
+        grep -q '^ID=ubuntu$' /etc/os-release || exit 1
+        cat /etc/os-release
+    fi
+
+    . $SRC_DIR/setup-ubuntu.sh
 else
     echo "Unsupported OS: $OS"
     exit 1
